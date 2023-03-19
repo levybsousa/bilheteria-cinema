1a48e05235e8c987719a2a059d22e50ad50b7872
levyzera <levy.edu.11@gmail.com>
2023-03-18 21:15:57 -0300

    adicionando pacotes

---
+++ src/main/java/bilhereteriacinema/BilheteriaApplication.java
@@ -0,0 +1,13 @@
+package bilhereteriacinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilheteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilheteriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bilhereteriacinema/BilheteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package bilhereteriacinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilheteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
