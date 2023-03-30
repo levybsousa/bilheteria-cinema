86b2fff1ac67ea46716b0b2d2d9a9c9f0dfffddf
levyzera <levy.edu.11@gmail.com>
2023-03-29 22:39:02 -0300

    correções

--- src/main/java/bilhereteriacinema/BilheteriaApplication.java
+++ src/main/java/bilhereteriacinema/BilheteriaApplication.java
@@ -2,12 +2,5 @@ package bilhereteriacinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class BilheteriaApplication {
-
-	public static void main(filme[] args) {
-		SpringApplication.run(BilheteriaApplication.class, args);
-	}
-
 }
