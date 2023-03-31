0d41cd4997cc6d4d5727a67aae4a0e95fef56530
levyzera <levy.edu.11@gmail.com>
2023-03-31 20:25:05 -0300

    atualizando projeto

--- src/main/java/bilhereteriacinema/BilheteriaApplication.java
+++ src/main/java/bilhereteriacinema/BilheteriaApplication.java
@@ -1,6 +1,12 @@
 package bilhereteriacinema;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+@SpringBootConfiguration
 public class BilheteriaApplication {
+    public static void main(String[] args) {
+
+    }
 }
