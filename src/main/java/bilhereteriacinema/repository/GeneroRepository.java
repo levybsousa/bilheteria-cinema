1c17f78155aeda3ab1ec967b44e05ed79c5cd9a5
levybsousa <levybsousa02@gmail.com>
2023-06-11 01:05:59 -0300

    iniciando classe GeneroRepository

---
+++ src/main/java/bilhereteriacinema/repository/GeneroRepository.java
@@ -0,0 +1,9 @@
+package bilhereteriacinema.repository;
+
+import bilhereteriacinema.model.entity.Genero;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GeneroRepository extends JpaRepository<Genero, Long> {
+}
