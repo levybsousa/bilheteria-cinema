3426424bbe02518f97a7cf292bf138ec51c01e0b
levybsousa <levybsousa02@gmail.com>
2023-06-11 00:48:19 -0300

    iniciando classe GeneroService

---
+++ src/main/java/bilhereteriacinema/service/GeneroService.java
@@ -0,0 +1,23 @@
+package bilhereteriacinema.service;
+
+import bilhereteriacinema.model.entity.Genero;
+
+import java.util.List;
+
+public class GeneroService {
+    public List<Genero> getAll(){
+        return null;
+    }
+    public Genero getById(Long id){
+        return null;
+    }
+    public Genero save(Genero genero){
+        return null;
+    }
+    public Genero update(Long id, Genero genero ){
+        return null;
+    }
+    public void delete(Long id){
+
+    }
+}
