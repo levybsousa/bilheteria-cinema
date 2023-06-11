87ece1de6b033c3de12cfaa1173a33dedd814469
levybsousa <levybsousa02@gmail.com>
2023-06-11 00:36:58 -0300

    iniciando classe GeneroEntity

---
+++ src/main/java/bilhereteriacinema/model/entity/Genero.java
@@ -0,0 +1,14 @@
+package bilhereteriacinema.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Genero {
+    private String tipoDoGenero;
+}
