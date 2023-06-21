5a4c93e3e280829f073e9e25ae16eb08dfe0d755
levybsousa <levybsousa02@gmail.com>
2023-06-21 12:49:08 -0300

    atualizando metodos classe GeneroService

--- src/main/java/bilhereteriacinema/service/GeneroService.java
+++ src/main/java/bilhereteriacinema/service/GeneroService.java
@@ -6,24 +6,41 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class GeneroService {
     private final GeneroRepository generoRepository;
 
-    public List<Genero> getAll(){
-        return null;
+    public List<Genero> getAll() {
+        List<Genero> result = generoRepository.findAll();
+        return result;
     }
-    public Genero getById(Long id){
-        return null;
-    }
-    public Genero save(Genero genero){
-        return null;
-    }
-    public Genero update(Long id, Genero genero ){
-        return null;
-    }
-    public void delete(Long id){
 
+    public Genero getById(Long id) {
+        Optional<Genero> result = generoRepository.findById(id);
+        if (result.isPresent()) {
+            return result.get();
+        } else {
+            throw new RuntimeException("Recurso não encontrado");
+        }
+    }
+
+    public Genero save(Genero genero) {
+        Genero result = generoRepository.save(genero);
+        return result;
+    }
+
+    public Genero update(Long id, Genero genero) {
+        getById(id);
+        genero.setId(id);
+        Genero result = generoRepository.save(genero);
+        return result;
+    }
+
+    public void delete(Long id) {
+        getById(id);
+        generoRepository.deleteById(id);
     }
 }
