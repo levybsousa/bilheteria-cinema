c60a14276044f0f7ab7a6bdab64908697febe6ec
levybsousa <levybsousa02@gmail.com>
2023-06-29 00:07:55 -0300

    Maven to gradle

--- src/main/java/bilhereteriacinema/exception/RecursoNaoEncontradoException.java
+++ src/main/java/bilhereteriacinema/exception/RecursoNaoEncontradoException.java
@@ -3,7 +3,6 @@ package bilhereteriacinema.exception;
 public class RecursoNaoEncontradoException extends RuntimeException {
 
     public RecursoNaoEncontradoException() {
-        super();
-        System.out.println("Recurso não encontrado");
+        super("Recurso não encontrado");
     }
 }
