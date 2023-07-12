94dfa6f65ca4bfaeb1ada2e420dd192fd8463de0
levybsousa <levybsousa02@gmail.com>
2023-07-12 15:03:55 -0300

    Criando interface ProdutoraRepository

---
+++ src/main/java/bilhereteriacinema/repository/ProdutoraRepository.java
@@ -0,0 +1,10 @@
+package bilhereteriacinema.repository;
+
+import bilhereteriacinema.model.entity.Produtora;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoraRepository extends JpaRepository<Produtora, Long> {
+
+}
