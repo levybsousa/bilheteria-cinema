502ef56f3040eb36912db0518a461292e4b8a3d6
levybsousa <levybsousa02@gmail.com>
2023-08-28 19:47:22 -0300

    atualizando classe FilmeController

--- src/main/java/bilhereteriacinema/controller/FilmeController.java
+++ src/main/java/bilhereteriacinema/controller/FilmeController.java
@@ -5,6 +5,7 @@ import bilhereteriacinema.model.entity.Filme;
 import bilhereteriacinema.service.FilmeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -23,18 +24,21 @@ public class FilmeController {
     }
 
     @GetMapping("/{id}")
-    public Filme getByID(@PathVariable Long id) {
-        return filmeService.getById(id);
+    public ResponseEntity<Filme> getByID(@PathVariable Long id) {
+        Filme filmeResponse = filmeService.getById(id);
+        return ResponseEntity.ok(filmeResponse);
     }
 
     @PostMapping
-    public Filme save(@RequestBody Filme filme) {
-        return filmeService.save(filme);
+    public ResponseEntity<Filme> save(@RequestBody Filme filme) {
+        Filme filmeResponse = filmeService.save(filme);
+        return ResponseEntity.ok(filmeResponse);
     }
 
     @PutMapping("/{id}")
-    public Filme update(@PathVariable Long id, @RequestBody Filme filme) {
-        return filmeService.update(id, filme);
+    public ResponseEntity<Filme> update(@PathVariable Long id, @RequestBody Filme filme) {
+        Filme filmeResponse = filmeService.update(id, filme);
+        return ResponseEntity.ok(filmeResponse);
     }
 
     @DeleteMapping("/{id}")
